97a38c926158a6ef2520c3fa760fa516068d9025
Phil Corbett <corbett.phil@gmail.com>
2019-08-15 09:34:02 -0400

    Update deployment and other things.

--- Tete.Db/test.sql
+++ Tete.Db/test.sql
@@ -1,12 +1,3 @@
--- Create a new database called 'Tete'
--- Connect to the 'master' database to run this snippet
-USE master
-GO
--- Create the new database if it does not exist already
-IF NOT EXISTS (
-  SELECT name
-    FROM sys.databases
-    WHERE name = N'Tete'
-)
-CREATE DATABASE Tete
-GO
+use Tete;
+
+select * from Flags;

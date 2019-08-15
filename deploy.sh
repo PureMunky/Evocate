c6d6dde465d994908e61e4320be318dd7d3a988a
Phil Corbett <corbett.phil@gmail.com>
2019-08-15 05:28:07 -0400

    Update k8s deployment

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,18 @@ docker rm tete-web
 dotnet clean
 
 # build a new version of core
+docker build -f Db.Dockerfile -t tete-db-img .
 docker build -f Api.Dockerfile -t tete-api-img .
 docker build -f Web.Dockerfile -t tete-web-img .
 
-kubectl apply -f tete-deployment.yml
+docker tag tete-db-img:latest puremunky/tete-db:latest
+docker tag tete-api-img:latest puremunky/tete-api:latest
+docker tag tete-web-img:latest puremunky/tete-web:latest
+
+docker push puremunky/tete-db:latest
+docker push puremunky/tete-api:latest
+docker push puremunky/tete-web:latest
+
+# kubectl apply -f tete-deployment.yml
 
 # ./deploy.sh
